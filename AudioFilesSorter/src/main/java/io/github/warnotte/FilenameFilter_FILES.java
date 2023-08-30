b55120d48d44e4890cb5bc170e5d0a21e20febd0
warnotte <renaud@warnotte.be>
2023-08-30 20:10:43 +0200

    Add Maven nature

---
+++ AudioFilesSorter/src/main/java/io/github/warnotte/FilenameFilter_FILES.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package io.github.warnotte;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+/**
+ * @author user
+ *
+ */
+public class FilenameFilter_FILES implements FilenameFilter
+{
+	public boolean accept(File dir, String name)
+	{
+		File file = new File(dir.getAbsolutePath()+"\\"+name);
+		
+		if (file.isDirectory()==true)
+			return false;
+	/*	if ((
+		//		file.getAbsolutePath().toLowerCase().endsWith("m3u") || 
+		//		file.getAbsolutePath().toLowerCase().endsWith("nfo") || 
+		//		file.getAbsolutePath().toLowerCase().endsWith("sfv") || 
+		//		file.getAbsolutePath().toLowerCase().endsWith("jpg") || 
+		//		file.getAbsolutePath().toLowerCase().endsWith("png") || 
+		//		file.getAbsolutePath().toLowerCase().endsWith("gif") 
+		))
+		return false;*/
+		return true;
+	}
+}
